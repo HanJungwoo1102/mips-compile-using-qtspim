eb49a7999287d833e0b263119e9da4ba1c5265c8
hanjungwoo <ajtwlswjddnv1102@gmail.com>
2021-04-04 15:21:08 +0900

    feat:pa1-2 print

--- pa1-1/pa1-1-function.s
+++ pa1-1/pa1-1-function.s
@@ -2,40 +2,40 @@ GCD:
 #-----------------------------------------------------
 # Your code "START" here
 #-----------------------------------------------------
-addi $sp, $sp, -8
-sw $s1, 4($sp)
-sw $s0, 0($sp)
-slt $t0, $a0, $a1
-bne $t0, $zero, PA1_1_GCD_IF_ELSE         # if not (n < m) -> else
-add $s1, $a0, $zero                       # j = n
-j PA1_1_GCD_IF_EXIT
+  addi $sp, $sp, -8
+  sw $s1, 4($sp)
+  sw $s0, 0($sp)
+  slt $t0, $a0, $a1
+  bne $t0, $zero, PA1_1_GCD_IF_ELSE         # if not (n < m) -> else
+  add $s1, $a0, $zero                       # j = n
+  j PA1_1_GCD_IF_EXIT
 
 PA1_1_GCD_IF_ELSE:
-add $s1, $a1, $zero                       # j = m
+  add $s1, $a1, $zero                       # j = m
 
 PA1_1_GCD_IF_EXIT:
-add $s0, $s1, $zero                       # i = j
+  add $s0, $s1, $zero                       # i = j
 
 PA1_1_GCD_FOR:
-slt $t0, $zero, $s0
-beq $t0, $zero, PA1_1_GCD_FOR_EXIT        # if (0 < i)
-div $a0, $s0                              
-mfhi $t0                                  # $t0 = n % i
-bne $t0, $zero PA1_1_GCD_IF_EXIT_2                           
-div $a1, $s0
-mfhi $t0                                  # $t0 = m % i
-bne $t0, $zero PA1_1_GCD_IF_EXIT_2    
-add $v0, $s0, $zero                       # ret = i
-j PA1_1_GCD_FOR_EXIT                      # break
+  slt $t0, $zero, $s0
+  beq $t0, $zero, PA1_1_GCD_FOR_EXIT        # if (0 < i)
+  div $a0, $s0                              
+  mfhi $t0                                  # $t0 = n % i
+  bne $t0, $zero PA1_1_GCD_IF_EXIT_2                           
+  div $a1, $s0
+  mfhi $t0                                  # $t0 = m % i
+  bne $t0, $zero PA1_1_GCD_IF_EXIT_2    
+  add $v0, $s0, $zero                       # ret = i
+  j PA1_1_GCD_FOR_EXIT                      # break
 
 PA1_1_GCD_IF_EXIT_2:
-addi $s0, $s0, -1                         # i += -1
-j PA1_1_GCD_FOR
+  addi $s0, $s0, -1                         # i += -1
+  j PA1_1_GCD_FOR
 
 PA1_1_GCD_FOR_EXIT:
-lw $s0, 0($sp)
-lw $s1, 4($sp)
-addi $sp, $sp, 8
+  lw $s0, 0($sp)
+  lw $s1, 4($sp)
+  addi $sp, $sp, 8
 #-----------------------------------------------------
 # Your code "END" here
 #-----------------------------------------------------
@@ -45,38 +45,38 @@ LCM:
 #-----------------------------------------------------
 # Your code "START" here
 #-----------------------------------------------------
-addi $sp, $sp, -8
-sw $s1, 4($sp)
-sw $s0, 0($sp)
-slt $t0, $a1, $a0
-bne $t0, $zero, PA1_1_LCM_IF_ELSE         # if not (n > m) -> else 로
-add $s1, $a0, $zero                       # j = n
-j PA1_1_LCM_IF_EXIT
+  addi $sp, $sp, -8
+  sw $s1, 4($sp)
+  sw $s0, 0($sp)
+  slt $t0, $a1, $a0
+  bne $t0, $zero, PA1_1_LCM_IF_ELSE         # if not (n > m) -> else 로
+  add $s1, $a0, $zero                       # j = n
+  j PA1_1_LCM_IF_EXIT
 
 PA1_1_LCM_IF_ELSE:
-add $s1, $a1, $zero                       # j = m
+  add $s1, $a1, $zero                       # j = m
 
 PA1_1_LCM_IF_EXIT:
-add $s0, $s1, $zero                       # i = j
+  add $s0, $s1, $zero                       # i = j
 
 PA1_1_LCM_FOR:
-div $s0, $a0                              
-mfhi $t0                                  # $t0 = i % n
-bne $t0, $zero PA1_1_LCM_IF_EXIT_2                           
-div $s0, $a1
-mfhi $t0                                  # $t0 = i % m
-bne $t0, $zero PA1_1_LCM_IF_EXIT_2    
-add $v0, $s0, $zero                       # ret = i
-j PA1_1_LCM_FOR_EXIT                      # break
+  div $s0, $a0                              
+  mfhi $t0                                  # $t0 = i % n
+  bne $t0, $zero PA1_1_LCM_IF_EXIT_2                           
+  div $s0, $a1
+  mfhi $t0                                  # $t0 = i % m
+  bne $t0, $zero PA1_1_LCM_IF_EXIT_2    
+  add $v0, $s0, $zero                       # ret = i
+  j PA1_1_LCM_FOR_EXIT                      # break
 
 PA1_1_LCM_IF_EXIT_2:
-add $s0, $s0, $s1                         # i += j
-j PA1_1_LCM_FOR
+  add $s0, $s0, $s1                         # i += j
+  j PA1_1_LCM_FOR
 
 PA1_1_LCM_FOR_EXIT:
-lw $s0, 0($sp)
-lw $s1, 4($sp)
-addi $sp, $sp, 8
+  lw $s0, 0($sp)
+  lw $s1, 4($sp)
+  addi $sp, $sp, 8
 #-----------------------------------------------------
 # Your code "END" here
 #-----------------------------------------------------
--- pa1-2/pa1-2-function.s
+++ pa1-2/pa1-2-function.s
@@ -2,7 +2,21 @@ product:
 #-----------------------------------------------------
 # Your code "START" here
 #-----------------------------------------------------
+  addi $sp, $sp, -12
+  sw $s2, 8($sp)
+  sw $s1, 4($sp)
+  sw $s0, 0($sp)
 
+  la $t0, A 
+  la $t1, B  
+  la $t2, C
+  la $t3, DIM
+  la $t4, N
+
+  lw $s0, 0($sp)
+  lw $s1, 4($sp)
+  lw $s2, 8($sp)
+  addi $sp, $sp, 12
 #-----------------------------------------------------
 # Your code "END" here
 #-----------------------------------------------------
@@ -12,7 +26,31 @@ printResult:
 #-----------------------------------------------------
 # Your code "START" here
 #-----------------------------------------------------
+  addi $sp, $sp, -8
+  sw $s1, 4($sp)
+  sw $s0, 0($sp)
 
+  la $t0, B  
+  la $t1, DIM
+
+  move $s0, $zero                               # i = 0
+  slt $t2, $s0, $t1                             # t2 = i < DIM
+  bne $t2, $zero, PRINE_FOR_1_END
+
+  move $s1, $zero                               # j = 0
+  slt $t2, $s1, $t1                             # t2 = j < DIM
+  bne $t2, $zero, PRINE_FOR_2_END
+
+
+PRINE_FOR_2_END:
+  li $v0, 4
+  la $a0, ENTER
+  syscall		                  # printf("\n")
+PRINE_FOR_1_END:
+
+  lw $s0, 0($sp)
+  lw $s1, 4($sp)
+  addi $sp, $sp, 8
 #-----------------------------------------------------
 # Your code "END" here
 #-----------------------------------------------------
