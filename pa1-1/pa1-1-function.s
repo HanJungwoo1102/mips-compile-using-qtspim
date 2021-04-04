5f2b9d83628ffc959bb58209b18acdfa1fb554a0
hanjungwoo <ajtwlswjddnv1102@gmail.com>
2021-04-04 12:23:08 +0900

    feat:gcd

--- pa1-1/pa1-1-function.s
+++ pa1-1/pa1-1-function.s
@@ -2,25 +2,40 @@ GCD:
 #-----------------------------------------------------
 # Your code "START" here
 #-----------------------------------------------------
-addi $sp, $sp, -12
-sw $s2, 8($sp)
+addi $sp, $sp, -8
 sw $s1, 4($sp)
 sw $s0, 0($sp)
 slt $t0, $a0, $a1
-beq $t0, $zero, PA1_1_1_ELSE
-add $s1, $a0, $zero
-j PA1_1_1_EXIT
+bne $t0, $zero, PA1_1_GCD_IF_ELSE         # if not (n < m) -> else
+add $s1, $a0, $zero                       # j = n
+j PA1_1_GCD_IF_EXIT
 
-PA1_1_1_ELSE:
-add $s1, $a1, $zero
-j EXIT
+PA1_1_GCD_IF_ELSE:
+add $s1, $a1, $zero                       # j = m
 
-PA1_1_1_EXIT:
-addi $v0, $zero, 8
+PA1_1_GCD_IF_EXIT:
+add $s0, $s1, $zero                       # i = j
+
+PA1_1_GCD_FOR:
+slt $t0, $zero, $s0
+beq $t0, $zero, PA1_1_GCD_FOR_EXIT        # if (0 < i)
+div $a0, $s0                              
+mfhi $t0                                  # $t0 = n % i
+bne $t0, $zero PA1_1_GCD_IF_EXIT_2                           
+div $a1, $s0
+mfhi $t0                                  # $t0 = m % i
+bne $t0, $zero PA1_1_GCD_IF_EXIT_2    
+add $v0, $s0, $zero                       # ret = i
+j PA1_1_GCD_FOR_EXIT                      # break
+
+PA1_1_GCD_IF_EXIT_2:
+addi $s0, $s0, -1                         # i += -1
+j PA1_1_GCD_FOR
+
+PA1_1_GCD_FOR_EXIT:
 lw $s0, 0($sp)
 lw $s1, 4($sp)
-lw $s2, 8($sp)
-addi $sp, $sp, 12
+addi $sp, $sp, 8
 #-----------------------------------------------------
 # Your code "END" here
 #-----------------------------------------------------
@@ -30,15 +45,38 @@ LCM:
 #-----------------------------------------------------
 # Your code "START" here
 #-----------------------------------------------------
-addi $sp, $sp, -12
-sw $s2, 8($sp)
+addi $sp, $sp, -8
 sw $s1, 4($sp)
 sw $s0, 0($sp)
-addi $v0, $zero, 3072
+slt $t0, $a1, $a0
+bne $t0, $zero, PA1_1_LCM_IF_ELSE         # if not (n > m) -> else 로
+add $s1, $a0, $zero                       # j = n
+j PA1_1_LCM_IF_EXIT
+
+PA1_1_LCM_IF_ELSE:
+add $s1, $a1, $zero                       # j = m
+
+PA1_1_LCM_IF_EXIT:
+add $s0, $s1, $zero                       # i = j
+
+PA1_1_LCM_FOR:
+div $s0, $a0                              
+mfhi $t0                                  # $t0 = i % n
+bne $t0, $zero PA1_1_GCD_IF_EXIT_2                           
+div $s0, $a1
+mfhi $t0                                  # $t0 = i % m
+bne $t0, $zero PA1_1_GCD_IF_EXIT_2    
+add $v0, $s0, $zero                       # ret = i
+j PA1_1_LCM_FOR_EXIT                      # break
+
+PA1_1_LCM_IF_EXIT_2:
+add $s0, $s0, $s1                         # i += j
+j PA1_1_LCM_FOR
+
+PA1_1_LCM_FOR_EXIT:
 lw $s0, 0($sp)
 lw $s1, 4($sp)
-lw $s2, 8($sp)
-addi $sp, $sp, 12
+addi $sp, $sp, 8
 #-----------------------------------------------------
 # Your code "END" here
 #-----------------------------------------------------
